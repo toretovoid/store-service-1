78d93698665d7f352e0b0c020f7a98cb7a7c0f7f
Edson Terceros <edson.terceros@avantica.net>
2019-06-11 05:54:04 -0400

    bootstrap persistir instancias

--- src/main/java/edu/umss/storeservice/DevBootstrap.java
+++ src/main/java/edu/umss/storeservice/DevBootstrap.java
@@ -4,10 +4,13 @@
 
 package edu.umss.storeservice;
 
+import edu.umss.storeservice.model.*;
 import edu.umss.storeservice.service.*;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
@@ -43,6 +46,72 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-
+        initData();
     }
+
+    private void initData() {
+        // MAQUINARIA category
+        Category maquinariaCategory = new Category();
+        maquinariaCategory.setCode("MAQ");
+        maquinariaCategory.setName("MAQUINARIA");
+        categoryService.save(maquinariaCategory);
+
+        // TRACTOR subcategory
+        SubCategory tractorSubCategory = new SubCategory();
+        tractorSubCategory.setCategory(maquinariaCategory);
+        tractorSubCategory.setCode("TRA");
+        tractorSubCategory.setName("TRACTOR");
+        subCategoryService.save(tractorSubCategory);
+
+        // Taller category
+        Category tallerCategory = new Category();
+        tallerCategory.setCode("TAL");
+        tallerCategory.setName("TALLER");
+        categoryService.save(tallerCategory);
+
+
+        // montacarga subcategory
+        SubCategory montacargaSubCategory = new SubCategory();
+        montacargaSubCategory.setCategory(maquinariaCategory);
+        montacargaSubCategory.setCode("MON");
+        montacargaSubCategory.setName("MONTACARGA");
+        subCategoryService.save(montacargaSubCategory);
+
+        // tractorKubota20 Item
+        Item tractorKubota20 = new Item();
+        tractorKubota20.setCode("TRACTOR-K-20");
+        tractorKubota20.setName("TRACTOR Kubota 20 HP");
+        tractorKubota20.setSubCategory(tractorSubCategory);
+        itemService.save(tractorKubota20);
+
+        // montacargaMitsubishi2T Item
+        Item montacargaMitsubishi2T = new Item();
+        montacargaMitsubishi2T.setCode("MON-M-2T");
+        montacargaMitsubishi2T.setName("MONTACARGA MITSUBISHI 2T");
+        montacargaMitsubishi2T.setSubCategory(montacargaSubCategory);
+        itemService.save(montacargaMitsubishi2T);
+
+        // Employee Employee
+        Employee john = new Employee();
+        john.setFirstName("Pepito");
+        john.setLastName("Perez");
+
+        // Position
+        Position position = new Position();
+        position.setName("CHAPERO");
+        positionService.save(position);
+
+        // contract
+        Contract contract = new Contract();
+        contract.setEmployee(john);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2019, Calendar.MARCH, 10);
+        contract.setInitDate(calendar.getTime());
+        contract.setPosition(position);
+
+        john.getContracts().add(contract);
+        employeeService.save(john);
+        //contractService.save(contract);
+    }
+
 }
